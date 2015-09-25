358be5edec6eb160582d8627e12994272d8794be
nashgul <sinp13@gmail.com>
2015-09-25 13:06:18 +0200

    bug en entrada errónea de datos arrglado

--- netmask.py
+++ netmask.py
@@ -4,38 +4,39 @@
 import re
 
 def entrada_de_ip():
-    entrada = raw_input("Introduce la dirección de red: ")
-    matchIp = re.search( r'(([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}))(\/){0,1}([0-9]{1,2}){0,1}', entrada )
-    if matchIp:
-        ip_principal = map(int, list(matchIp.group(2,3,4,5)))
-        for x in ip_principal:
-            if x > 255 or x < 0:
-                print "ip incorrecta!!"
-                entrada_de_ip()
-        if matchIp.group(7):
-            mascara = int(matchIp.group(7))
-            if mascara > 32 or mascara < 0:
-                print "máscara incorrecta"
-                entrada_de_ip()
+    ip_principal = ()
+    while not ip_principal:
+        entrada = raw_input("Introduce la dirección de red: ")
+        matchIp = re.search( r'(([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}))(\/){0,1}([0-9]{1,2}){0,1}', entrada )
+        if matchIp:
+            ip_principal = map(int, list(matchIp.group(2,3,4,5)))
         else:
-            mascara = pedir_mascara()
+            print "Dirección ip inválida!"
+    for x in ip_principal:
+        if x > 255 or x < 0:
+            print "Valor %i incorrecto!!", x
+            entrada_de_ip()
+    if matchIp.group(7):
+        mascara = int(matchIp.group(7))
+        if mascara > 32 or mascara < 0:
+            print "máscara CIDR: %i incorrecta", mascara
+            entrada_de_ip()
     else:
-        print "Dirección ip inválida!"
-        entrada_de_ip()
+        mascara = pedir_mascara()
     return ip_principal, mascara
 
 def pedir_mascara():
-    entrada_mascara = raw_input("Introduce la máscara de red: ")
-    matchMask = re.search( r'(([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}))', entrada_mascara )
-    if not matchMask:
-        print "La máscara introducida no es válida"
-        pedir_mascara()
-    else:
-        mascara = map(int, list(matchMask.group(2,3,4,5)))
-        for x in mascara:
-            if x > 255 or x < 0:
-                print "máscara incorrecta!!"
-                pedir_mascara()
+    matchMask = ()
+    while not matchMask:
+        entrada_mascara = raw_input("Introduce la máscara de red: ")
+        matchMask = re.search( r'(([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}))', entrada_mascara )
+        if not matchMask:
+            print "La máscara introducida no es válida"    
+    mascara = map(int, list(matchMask.group(2,3,4,5)))
+    for x in mascara:
+        if x > 255 or x < 0:
+            print "máscara incorrecta!!"
+            pedir_mascara()
     return mascara
 
 def convertir(ip_address):
