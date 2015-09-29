dd28f94ff2912af04293e94668bb814dd40a6910
nashgul <sinp13@gmail.com>
2015-09-29 16:14:25 +0200

    pequeño error en los índices de la salida formateada arreglado

--- netmask.py
+++ netmask.py
@@ -107,7 +107,7 @@ if not sys.argv[1:]:
     ip_addresses.append(convertir_a_decimal(ip_addresses_bin[3]))
 
 print("")
-print("ip principal:\t\t{0:03d}.{1:03d}.{1:03d}.{1:03d}".format(ip_addresses[0][0],ip_addresses[0][1],ip_addresses[0][2],ip_addresses[0][3]))
+print("ip principal:\t\t{0:03d}.{1:03d}.{2:03d}.{3:03d}".format(ip_addresses[0][0],ip_addresses[0][1],ip_addresses[0][2],ip_addresses[0][3]))
 print("máscara de red: \t{0:03d}.{1:03d}.{2:03d}.{3:03d}".format(ip_addresses[1][0],ip_addresses[1][1],ip_addresses[1][2],ip_addresses[1][3]))
 print("ip inferior: \t\t{0:03d}.{1:03d}.{2:03d}.{3:03d}".format(ip_addresses[2][0],ip_addresses[2][1],ip_addresses[2][2],ip_addresses[2][3]))
 print("ip superior: \t\t{0:03d}.{1:03d}.{2:03d}.{3:03d}".format(ip_addresses[3][0],ip_addresses[3][1],ip_addresses[3][2],ip_addresses[3][3]))
